f3df4a56bcf61dcc11c59e7bd22794bf03591007
gpetuhov <g2p@bk.ru>
2018-02-08 22:16:30 +0300

    Add MessageProvider Java library module

--- mainactivitylibrary/src/main/java/com/gpetuhov/android/mainactivitylibrary/MainActivity.java
+++ mainactivitylibrary/src/main/java/com/gpetuhov/android/mainactivitylibrary/MainActivity.java
@@ -2,6 +2,9 @@ package com.gpetuhov.android.mainactivitylibrary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.gpetuhov.android.messageprovider.MessageProvider;
 
 
 // This activity class is declared inside Android library module
@@ -11,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        String message = new MessageProvider(1).getMessage();
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
